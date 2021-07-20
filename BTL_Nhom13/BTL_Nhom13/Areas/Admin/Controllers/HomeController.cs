19dd65cbc25032285c8dbc243e1505671999be77
Luong Ba Hoang <hoang2kthptlk@gmail.com>
2021-07-20 14:37:03 +0700

    Layout for admin page

---
+++ BTL_Nhom13/BTL_Nhom13/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTL_Nhom13.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult Category()
+        {
+            return View();
+        }
+        public ActionResult Receipt()
+        {
+            return View();
+        }
+        public ActionResult Account()
+        {
+            return View();
+        }
+    }
+}
