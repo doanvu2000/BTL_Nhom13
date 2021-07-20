706c51846d6a0bc2b85c76437e78666cc5752560
doanvu2000 <doanvu20122000@gmail.com>
2021-07-20 20:39:36 +0700

    update

--- BTL_Nhom13/BTL_Nhom13/Controllers/HomeController.cs
+++ BTL_Nhom13/BTL_Nhom13/Controllers/HomeController.cs
@@ -4,16 +4,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using PagedList;
 namespace BTL_Nhom13.Controllers
 {
     public class HomeController : Controller
     {
         TinhDauDB db = new TinhDauDB();
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder, int? madm, int? beginPrice, int? endPrice,string searchString,int? page)
         {
-            var sp = db.SanPhams.Select(s => s);
-            return View(sp);
+            int pageSize = 6;
+            int pageNumber = (page ?? 1);
+            var sp = db.SanPhams.ToList();
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                sp = (List<SanPham>)sp.Where(s => s.TenSP.Contains(searchString) );
+                return View(sp.ToList());
+            }
+            if (madm > 0)
+            {
+                sp = (List<SanPham>)sp.Where(s => s.MaDM == madm);
+            }
+            if (sortOrder != null)
+            {
+                switch (sortOrder)
+                {
+                    case "sortSL":
+                        sp = (List<SanPham>)sp.OrderBy(s => s.SoLuongTon); break;
+                    case "giaTang":
+                        sp = (List<SanPham>)sp.OrderBy(s => s.Gia); break;
+                    case "giaGiam":
+                        sp = (List<SanPham>)sp.OrderByDescending(s => s.Gia); break;
+                }
+            }
+            if (beginPrice > 0 && endPrice == 0)
+            {
+                sp = (List<SanPham>)sp.Where(s => s.Gia <= beginPrice).OrderBy(s => s.Gia);
+            }
+            if (beginPrice > 0 && endPrice > 0)
+            {
+                sp = (List<SanPham>)sp.Where(s => s.Gia >= beginPrice && s.Gia <= endPrice).OrderBy(s => s.Gia);
+            }
+            if (beginPrice == 0 && endPrice > 0)
+            {
+                sp = (List<SanPham>)sp.Where(s => s.Gia >= endPrice).OrderBy(s => s.Gia);
+            }
+            return View(sp.ToPagedList(pageNumber,pageSize));
         }
 
         public ActionResult Login()
