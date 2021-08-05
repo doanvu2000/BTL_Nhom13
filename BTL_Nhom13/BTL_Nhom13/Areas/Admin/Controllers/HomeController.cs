d04dad52a2d1494a807d7b58319423027ae0e18e
Luong Ba Hoang <hoang2kthptlk@gmail.com>
2021-08-05 09:36:49 +0700

    Fix calculate total money of each receipt and change dashboard page.

--- BTL_Nhom13/BTL_Nhom13/Areas/Admin/Controllers/HomeController.cs
+++ BTL_Nhom13/BTL_Nhom13/Areas/Admin/Controllers/HomeController.cs
@@ -16,10 +16,41 @@ namespace BTL_Nhom13.Areas.Admin.Controllers
         // GET: Admin/Home
         public ActionResult Index()
         {
-            if(Session["TenTaiKhoan"] == null)
+            if (Session["TenTaiKhoan"] == null)
             {
                 return RedirectToAction("Login");
             }
+            var products = db.SanPhams.ToList();
+            var categories = db.DanhMucs.ToList();
+            var receipts = db.HoaDons.ToList();
+            DateTime today = DateTime.Today;
+            List<HoaDon> hds = db.HoaDons.Where(h => h.NgayDat.Month == today.Month &&
+                    h.NgayDat.Year == today.Year).ToList();
+            decimal tongTienNum = 0;
+            foreach (var item in hds)
+            {
+                tongTienNum += item.GioHang.ChiTietGioHangs.Select(c => c.SoLuongMua * c.Gia).Sum();
+            }
+
+            List<HoaDon> hd_trong_nam = hds.Where(h => h.NgayDat.Year == today.Year).ToList();
+            decimal tongTienTrongNamNum = 0;
+            foreach (var item in hd_trong_nam)
+            {
+                tongTienTrongNamNum += item.GioHang.ChiTietGioHangs.Select(c => c.SoLuongMua * c.Gia).Sum();
+            }
+
+            var accounts = db.TaiKhoans.Where(t => t.Quyen == 0).ToList();
+
+            ViewBag.soLuongSp = products.Count;
+            ViewBag.soLuongDm = categories.Count;
+            ViewBag.soLuongDh = hds.Count;
+            ViewBag.soLuongTk = accounts.Count;
+            System.Globalization.CultureInfo cul = System.Globalization.CultureInfo.GetCultureInfo("vi-VN");   // try with "en-US"
+            string tongTien = tongTienNum.ToString("#,###", cul.NumberFormat);
+            string tongTienTrongNam = tongTienTrongNamNum.ToString("#,###", cul.NumberFormat);
+            ViewBag.tongTienThangNay = tongTien;
+            ViewBag.tongTienTrongNam = tongTienTrongNam;
+
             return View();
         }
         [HttpGet]
@@ -40,7 +71,7 @@ namespace BTL_Nhom13.Areas.Admin.Controllers
                 ViewBag.ErrorMatKhau = "Mật khẩu không được để trống";
             }
 
-            var user = db.TaiKhoans.Where(t => t.TenTaiKhoan.Equals(TenTaiKhoan) && 
+            var user = db.TaiKhoans.Where(t => t.TenTaiKhoan.Equals(TenTaiKhoan) &&
             t.MatKhau.Equals(MatKhau) && t.Quyen == 1 && t.TinhTrang == true).ToList();
             if (user.Count() > 0)
             {
